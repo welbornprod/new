f670564465e6fb9fe6fecd6a8e48e7afdc7f271a
Christopher Welborn <cj@welbornprod.com>
2015-01-19 16:33:45 -0600

    Added basic rust plugin.

---
+++ plugins/rust.py
@@ -0,0 +1,30 @@
+""" Rust plugin for New
+    -Christopher Welborn 1-19-15
+"""
+
+from plugins import Plugin
+
+# Not much in this plugin at the moment.
+
+
+class RustPlugin(Plugin):
+
+    """ Creates a blank Rust file. """
+
+    def __init__(self):
+        self.name = ('rust', 'rs')
+        self.extensions = ('.rs',)
+        self.ignore_post = ('chmodx',)
+        self.version = '1.0.0'
+
+    def create(self, fname, args):
+        """ Creates a blank Rust file. """
+        return """use std::io;
+
+fn main() {
+
+}
+"""
+
+
+plugins = (RustPlugin(),)
