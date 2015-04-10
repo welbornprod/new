29e89701a0c863607978a21357f12b526c261834
Christopher Welborn <cj@welbornprod.com>
2015-04-09 21:17:01 -0500

    Added PHP plugin for fast oddity testing.

---
+++ plugins/php.py
@@ -0,0 +1,41 @@
+""" PHP plugin for New.
+    Creates executable PHP scripts.
+    -Christopher Welborn 12-25-14
+"""
+import os.path
+from plugins import Plugin, date
+
+# I'm not a php developer, and I wouldn't recommend it to someone wanting
+# to learn about programming.
+# However, when I run across php "oddities" I like to test them out.
+# That's why this plugin exists.
+template = """#!/usr/bin/env php
+<?php
+
+/*  {name}
+    ...
+    {author}{date}
+*/
+
+
+"""
+
+
+class PhpPlugin(Plugin):
+
+    def __init__(self):
+        self.name = ('php',)
+        self.extensions = ('.php',)
+        self.version = '0.0.1'
+        self.description = 'Creates an executable php script.'
+        self.load_config()
+
+    def create(self, filename):
+        """ Creates an executable php script. """
+        basename = os.path.split(filename)[-1]
+        author = self.config.get('author', '')
+        if author:
+            author = '-{} '.format(author)
+        return template.format(name=basename, author=author, date=date())
+
+exports = (PhpPlugin(),)
